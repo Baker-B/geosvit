a69d9d5d635c578480b46f00421e65c5094487e1
baker_a <akalishuk@gmail.com>
2021-03-21 15:44:39 +0200

    http links fix

--- vinton/ips.php
+++ vinton/ips.php
@@ -4,7 +4,7 @@ require_once("ips.inc.php" );
 
 $redirect = "
 //document.body.bgcolor = '#FFFFFF';
-document.write('<noindex><br><br><div align=\"center\"><table border=\"0\" width=\"32%\" cellspacing=\"0\" cellpadding=\"0\"><tr><td width=\"450\"><img border=\"0\" src=\"http://shinyshiny.ru/images/novsite.jpg\" width=\"450\" height=\"378\"></td></tr></table><table width=\"620\" border=\"0\"><tr><td><div align=\"left\"><a href=\'\' onClick=\"this.href=\'http://google.com\'\" rel=\"nofollow\"><img border=\"0\" src=\"http://shinyshiny.ru/images/logo_2.gif\" width=\"620\" height=\"64\"></a> </div></td></tr></table><img src=\"http://shinyshiny.ru/images/img01.jpg\" alt=\"\" /></div></noindex>');
+document.write('<noindex><br><br><div align=\"center\"><table border=\"0\" width=\"32%\" cellspacing=\"0\" cellpadding=\"0\"><tr><td width=\"450\"><img border=\"0\" src=\"https://shinyshiny.ru/images/novsite.jpg\" width=\"450\" height=\"378\"></td></tr></table><table width=\"620\" border=\"0\"><tr><td><div align=\"left\"><a href=\'\' onClick=\"this.href=\'https://google.com\'\" rel=\"nofollow\"><img border=\"0\" src=\"https://shinyshiny.ru/images/logo_2.gif\" width=\"620\" height=\"64\"></a> </div></td></tr></table><img src=\"htts://shinyshiny.ru/images/img01.jpg\" alt=\"\" /></div></noindex>');
 document.write('<div style="padding-top:75%">');
 ";
 
